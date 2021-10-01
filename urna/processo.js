ab208bf0b8e33541e95b3af516a6830e30951260
Denise Risia <risiaresponde@gmail.com>
2021-10-01 18:07:25 -0300

    Urna Eletronica html/css

---
+++ urna/processo.js
@@ -0,0 +1,53 @@
+let processo = [
+
+/* PRIMEIRO - VEREADOR */
+{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'candidato.jpg',legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+/* SEGUNDO - PREFEITO */
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'candidato.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    {url: 'candidato.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'candidato.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
